394770a965abd6e36b448498c964c4a9995a6cb8
krakdustten <gybelsdylan@gmail.com>
2016-04-25 11:33:58 +0200

    layout complete

---
+++ mvcforum-master/MVCForum.Website/Controllers/Elektronica_webpaginasController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using MVCForum.Domain.DomainModel.Entities;
+using MVCForum.Domain.Interfaces.Services;
+using MVCForum.Domain.Interfaces.UnitOfWork;
+using MVCForum.Website.ViewModels;
+
+namespace MVCForum.Website.Controllers
+{
+    public class Elektronica_webpaginasController : BaseController
+    {
+        public Elektronica_webpaginasController(ILoggingService loggingService, IUnitOfWorkManager unitOfWorkManager, IMembershipService membershipService,
+           ILocalizationService localizationService, IRoleService roleService, ISettingsService settingsService) : 
+            base(loggingService, unitOfWorkManager, membershipService, localizationService, roleService, settingsService)
+        {
+            
+        }
+        public ActionResult algemene_elektronica()
+        {
+            return View();
+        }
+    }
+}
